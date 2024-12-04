3698998a2a90a2f107eee6dfbb5f0d8c08a7dc9e
Jiake Wang <149813501+RastyASWoz@users.noreply.github.com>
2024-12-04 10:35:43 +0800

    更新测试文件

--- tests/t.py
+++ tests/t.py
@@ -1,12 +1,12 @@
 import requests
 
-# file_path = "C:\\Users\\33029\\Music\\vlc-record-2024-09-27-23h10m36s-moon gun-.wav"
+file_path = ""
 
-# # 测试recv接口
-# url = "http://127.0.0.1:5000/recv"
-# files = {'file': open(file_path, 'rb')}
-# response = requests.post(url, files=files)
-# print(response.json())
+# 测试recv接口
+url = "http://127.0.0.1:5000/recv"
+files = {'file': open(file_path, 'rb')}
+response = requests.post(url, files=files)
+print(response.json())
 
 # 测试send接口
 url = "http://127.0.0.1:5000/send"
